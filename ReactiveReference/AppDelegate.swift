3527f6965f789fc7963d4481f47bb8d4eadf4764
snlo <snloveydus@sina.com>
2019-09-24 11:59:17 +0800

    update
    Xcode 11 向后兼容 iOS 8.0

--- ReactiveReference/AppDelegate.swift
+++ ReactiveReference/AppDelegate.swift
@@ -12,20 +12,33 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
+    var window: UIWindow?
 
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        
+//        window?.rootViewController = ReactiveViewController.init()
+        window?.rootViewController = RxViewController.init()
+        
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
+    @available (iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
